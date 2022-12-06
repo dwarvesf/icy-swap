372b3fb5c2f7661da898a45dae621c42d51f9193
tuanddd <vincenttuandao@gmail.com>
2022-12-06 17:03:18 +0700

    chore: add icy emoji

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -14,14 +14,18 @@ const moneyEmoji = "<a:money:1049621199468105758>";
 export default function handler(req: NextApiRequest, res: NextApiResponse<{}>) {
   const { address, value: _value, tx } = req.query;
   const value = Number(_value);
+  const valueUsd = value / RATE;
 
   const embed = new EmbedBuilder()
     .setColor("#c2e2ec")
     .setTimestamp()
     .setDescription(
-      `\n${moneyEmoji} \`${address?.slice(0, 5)}...${address?.slice(
-        -5
-      )}\` just claimed ${value} ICY for ${
+      `${Array(Math.ceil(valueUsd / 20))
+        .fill(icyEmoji)
+        .join("")}\n\n${moneyEmoji} \`${address?.slice(
+        0,
+        5
+      )}...${address?.slice(-5)}\` just claimed ${value} ICY for ${
         value * RATE
       } USDC\n:chains: Transaction: [Polygonscan](${tx})\n\n:robot: Head to [earn.d.foundation](https://earn.d.foundation) to see available quests and r&d topics.`
     )
