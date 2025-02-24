53195d6e84247a6ebf66c5292b137b52098eefa8
vincent <vincenttuandao@gmail.com>
2025-02-24 10:22:48 +0700

    fix: rename envs

--- envs.ts
+++ envs.ts
@@ -4,8 +4,5 @@ export const ICY_CONTRACT_ADDRESS = process.env
 export const ICY_SWAPPER_CONTRACT_ADDRESS = process.env
   .NEXT_PUBLIC_ICY_SWAPPER_CONTRACT_ADDRESS as `0x${string}`;
 
-export const BASE_URL = process.env.NEXT_PUBLIC_FORTRESS_API as string;
+export const BASE_URL = process.env.NEXT_PUBLIC_BE_API as string;
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
-
-export const WEBHOOK_ID = process.env.WEBHOOK_ID as string;
-export const WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN as string;
